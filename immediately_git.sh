8d1f97ea999c9b6c300bbd46c20e911bb38f9f7d
robin_sky <robin_sky@win11.com>
2025-04-25 09:21:54 +0800

    20250425092154

---
+++ immediately_git.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo 'start'
+pwd
+echo 'git pull origin main'
+git pull origin main
+
+echo 'git status'
+git status
+
+echo 'git add *'
+git add -A
+cur_date="git commit -a -m "`date +%Y%m%d%H%M%S`
+echo $cur_date
+eval $cur_date
+
+echo 'git push origin main'
+git push origin main
+echo ---------------------------------------
+echo 'git status'
+git status
+echo 按任意键退出
+read -n 1
+echo exit
